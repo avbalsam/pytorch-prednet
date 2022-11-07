059f19fa81beff0ef03a4bd5b03bd068143f5eb9
avbalsam <avbalsam@gmail.com>
2022-11-07 17:19:44 -0500

    Add .sh file to run on openmind

--- run_mnist_openmind.sh
+++ run_mnist_openmind.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
-SBATCH -t 01:30:00          # walltime = 1 hours and 30 minutes
-SBATCH -n 1                 # one CPU (hyperthreaded) cores
-hostname                     # Print the hostname of the compute node
-# Execute commands to run your program here, taking Python for example,
-python mnist_train.py
+#SBATCH -t 24:00:00
+#SBATCH -n 1 # number of tasks to be launched (can't be smaller than -N)
+#SBATCH -c 4 # number of CPU cores associated with one GPU
+#SBATCH --gres=gpu:1 # number of GPUs
+##SBATCH --constraint=high-capacity
+##SBATCH --constraint=32GB
+##SBATCH --mem=16GB
+#SBATCH --array=1
+#SBATCH -D /om2/user/avbalsam/prednet/logs
+cd /om2/user/avbalsam/
+hostname
+date "+%y/%m/%d %H:%M:%S"
+# module load openmind/singularity/3.4.1
+# module add openmind/cuda/11.3
+# module add openmind/cudnn/11.5-v8.3.3.40
+# source /home/jangh/.bashrc
+# conda activate openmind
+source /om2/user/jangh/miniconda/etc/profile.d/conda.sh
+conda activate openmind
+python prednet/mnist_train.py
+# singularity exec --nv -B /om,/om2 /om2/user/xboix/singularity/xboix-tensorflow2.9b.simg \
+# python prednet/mnist_train.py \
+# --is_slurm=True \
+# --job=${SLURM_ARRAY_JOB_ID} \
+# --id=${SLURM_ARRAY_TASK_ID} \
